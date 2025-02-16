5030c6e2b62af40623487b149bcd4319790efe3d
Rao SHahbaz <menlenon59@gmail.com>
2025-02-16 15:34:24 +0500

    Changing Avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,12 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center bg-white shadow-md p-4 rounded-lg mb-6">
       <h1 className="text-xl font-semibold">Order Management</h1>
-      <UserButton />
+      <UserButton  appearance={{
+                // For Changing Avatar
+                elements:{
+                  avatarBox:"w-12 h-12"
+                }
+              }}/>
     </header>
   );
 }
